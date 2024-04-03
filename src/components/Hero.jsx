e5616328a7bb6f3093638283af9930a161ba405d
mtanng9 <mlt152498@gmail.com>
2024-04-03 13:13:11 -0400

    finished the cta section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,10 @@ function Hero () {
 
     return (
         <section id='hero' className='flex flex-col relative h-[600px]'>
-            <h1 className='text-3xl font-bold p-2'>Best Sellers</h1>
+            <h1 className='text-2xl font-bold p-2'>Best Sellers</h1>
             <h2 className='text-center text-2xl font-bold'>Authentic Snacks From Each Country</h2>
-            <a href="#" className='absolute bottom-0 p-3 ml-10 mb-10 rounded-lg bg-jordyBlue font-bold text-3xl'>Explore</a>
+            <a href="#" className='absolute bottom-0 p-2 ml-10 mb-10 rounded-lg
+             bg-jordyBlue font-bold text-1xl'>Explore</a>        
         </section>
     )
 }
