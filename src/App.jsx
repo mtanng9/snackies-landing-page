db0ce6b567980946e3b795e6a483b0ed82d7c44b
mtanng9 <mlt152498@gmail.com>
2024-04-03 12:18:09 -0400

    basic styling for what makes us different section

--- src/App.jsx
+++ src/App.jsx
@@ -14,19 +14,32 @@ function App() {
     </div>
 
     <div>
-      <section id='cta' className='flex flex-col'>
-          <div>
-            <h1> WHAT MAKES US DIFFERENT ?</h1>
-            <h2 className=''> Bringing The World's Snacks to Your Doorstep</h2>
-            <p> Enjoy Snacks from Comfort of Your Own Home! Curated and Packed with Love by Locals</p>
-            <img src="" alt="Picture of People Happy" />
-            <a href="#" className=''>Shop</a>
+      <section id='cta' className='flex flex-row p-3 space-x-3'>
+          <div className='flex flex-row'>
+            <div>
+             <img src="" alt="Picture of People Happy" />
+            </div>
+
+            <div>
+              <h1 className='mb-1 text-sm text-center'> WHAT MAKES US DIFFERENT ?</h1>
+              <h2 className='mb-1 font-bold text-lg'> Bringing The World's Snacks to Your Doorstep</h2>
+              <p className='mb-4 text-sm text-justify'> Enjoy Snacks from Comfort of Your Own Home! Curated and Packed with Love by Locals</p>
+
+              <div className='text-right'>
+                <a href="#" >Shop</a>
+              </div>
+            </div>
           </div>
 
           <div>
-            <p>Connect with Fellow Foodies, Share Recommendations, and Embark on a Culinary Journey Together!</p>
-            <a href="#">Create Account</a>
-            <a href="#">Explore</a>
+            <div>
+              <p>Connect with Fellow Foodies, Share Recommendations, and Embark on a Culinary Journey Together!</p>
+            </div>
+
+            <div>
+              <a href="#">Create Account</a>
+              <a href="#">Explore</a>
+            </div>
           </div>
         </section>
     </div>
