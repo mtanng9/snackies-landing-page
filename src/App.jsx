02b19dc0f152b917660070953fce861b06b22810
mtanng9 <mlt152498@gmail.com>
2024-04-03 11:48:36 -0400

    added html for cta

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,24 @@ function App() {
 
       <Navbar/>
       <Hero/>
+    </div>
 
+    <div>
+      <section id='cta' className='flex flex-col'>
+          <div>
+            <h1> WHAT MAKES US DIFFERENT ?</h1>
+            <h2 className=''> Bringing The World's Snacks to Your Doorstep</h2>
+            <p> Enjoy Snacks from Comfort of Your Own Home! Curated and Packed with Love by Locals</p>
+            <img src="" alt="Picture of People Happy" />
+            <a href="#" className=''>Shop</a>
+          </div>
 
+          <div>
+            <p>Connect with Fellow Foodies, Share Recommendations, and Embark on a Culinary Journey Together!</p>
+            <a href="#">Create Account</a>
+            <a href="#">Explore</a>
+          </div>
+        </section>
     </div>
     </>
   )
